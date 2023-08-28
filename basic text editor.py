90bd90dc60d690d64e714de13cb8bed4ccbc479b
Inayatt09 <140435227+Inayatt09@users.noreply.github.com>
2023-08-28 22:03:51 +0530

    Add files via upload

---
+++ basic text editor.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+from tkinter import filedialog
+
+def open_file():
+    file_path = filedialog.askopenfilename(filetypes=[("Text Files", "*.txt")])
+    if file_path:
+        with open(file_path, "r") as file:
+            text.delete(1.0, tk.END)
+            text.insert(tk.END, file.read())
+
+def save_file():
+    file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text Files", "*.txt")])
+    if file_path:
+        with open(file_path, "w") as file:
+            text_content = text.get(1.0, tk.END)
+            file.write(text_content)
+
+def clear_text():
+    text.delete(1.0, tk.END)
+
+
+root = tk.Tk()
+root.title("Basic Text Editor")
+
+
+text = tk.Text(root, wrap=tk.WORD)
+text.pack(expand=True, fill="both")
+
+
+menu_bar = tk.Menu(root)
+root.config(menu=menu_bar)
+
+
+file_menu = tk.Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="Open", command=open_file)
+file_menu.add_command(label="Save", command=save_file)
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=root.quit)
+
+
+edit_menu = tk.Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Edit", menu=edit_menu)
+edit_menu.add_command(label="Clear", command=clear_text)
+
+
+root.mainloop()
